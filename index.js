3c788fbeabffd12ca7fcc0c6847c8d445939801b
Bernardo Alonso <bernardo4430@gmail.com>
2024-01-31 11:51:21 -0600

    finished all logic and fixed some bugs

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-const {getUserInput} = require("./lib/inputHandler");
+const { getUserInput } = require('./lib/inputHandler');
+const { writeSVGToFile, startServer } = require('./lib/fileWriter');
+const { Triangle, Circle, Square } = require('./lib/shapes');
+
+async function generateLogo() {
+    const userInput = await getUserInput();
+
+    const shape = userInput.shape === 'Circle' ? new Circle() :
+                    userInput.shape === 'Triangle' ? new Triangle() :
+                    new Square();
+    shape.setColor(userInput.shapeColor);
+
+    const svgContent = shape.render();
+    const text = userInput.text || ""; // Get the user input text
+    const textColor = userInput.textColor || "black";
+    writeSVGToFile(svgContent,text, textColor);
+}
+
+generateLogo();
